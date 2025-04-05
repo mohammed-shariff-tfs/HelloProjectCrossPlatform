8365edc689c0cc9a66613c3179150cb5f2adb7f2
mohammed-shariff-tfs <mohammedjunaid.shariff@thermofisher.com>
2025-04-05 13:19:33 +0530

    Updated code

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_header.hpp"
+
+int main() {
+    say_hello();
+    std::cout << "Hello from main!" << std::endl;
+    return 0;
+}
---
+++ src/my_header.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "my_header.hpp"
+
+void say_hello() {
+    std::cout << "Hello from header!" << std::endl;
+}
